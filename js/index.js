8480ae9023b09d318efe1e35f46741474a10a402
lee-ann36 <readleeann@gmail.com>
2025-04-02 15:44:41 +0200

    d
    d

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function writeName(){
+    let name = document.getElementById('name');
+    let password = document.getElementById('password');
+    let welkom = document.getElementById('welkom');
+    let user = "juju";
+    let wacht = "edd";
+   console.log(name.value);
+    console.log(password.value);
+
+    if(name == user){
+        console.log("shi")
+    }
+
+  
+
+
+} 
+ if(password == "edd"){
+        welkom.innerHTML = "poop";
+    } 
+   
---
+++ js/test.js
@@ -0,0 +1,14 @@
+//const username = document.getElementById("username");
+//const password = document.getElementById("password");
+//const login = document.getElementById("Login")
+let username2;
+
+/*login.addEventListener("click", function(){
+ username2 = document.getElementById("username").value;
+console.log(username2);
+});*/
+
+document.getElementById("login").onclick = function(){
+    username2 = document.getElementById("username").value;
+    console.log(username2);
+}
